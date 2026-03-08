86c1760ff3e71d4186fb33b3a66ee3b346e1e16e
Cody Chaney <cody.chaney@snhu.edu>
2026-03-08 13:46:00 -0400

    initial commit

---
+++ Source/GAM315_Chaney/GAM315_Chaney.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM315_Chaney.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM315_Chaney, "GAM315_Chaney" );
